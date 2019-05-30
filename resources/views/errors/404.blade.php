aa2fa1e78f3e15c023e96576c657523bfc059d60
Mamisoa <oliviermamisoa@gmail.com>
2019-05-30 17:17:07 +0300

    mis en place de template d'eurreur et de la pages 404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,6 @@
+@extends('errors/template/default')
+
+
+@section('code',404)
+@section('header',"Page non trouvé")
+@section('message',"Désolé , la page que vous recherché a été supprimé ou n'existe pas")
