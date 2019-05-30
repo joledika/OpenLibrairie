f2f868e27e8b21f8735dd1c1925cb9694cf6af6c
Mamisoa <oliviermamisoa@gmail.com>
2019-05-30 17:51:41 +0300

    mis en place de page d'erreur 500

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,6 @@
+@extends('errors/template/default')
+
+
+@section('code',500)
+@section('header',"Erreur")
+@section('message',"Désolé , le serveur a subit une petite erreur")
--- resources/views/errors/template/default.blade.php
+++ resources/views/errors/template/default.blade.php
@@ -7,7 +7,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 
-	<title>{{env('APP_NAME')}} | Page d'eurreur</title>
+	<title>{{env('APP_NAME')}} | Page d'erreur</title>
 
 	<!-- Google font -->
 	<link href="https://fonts.googleapis.com/css?family=Nunito:400,700" rel="stylesheet">
