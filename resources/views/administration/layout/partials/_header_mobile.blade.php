f95ddabe39ff8c456b8141dff44f48c846fb5f9d
Mamisoa <oliviermamisoa@gmail.com>
2019-06-05 19:55:47 +0300

    modification du sidebar sur mobile

--- resources/views/administration/layout/partials/_header_mobile.blade.php
+++ resources/views/administration/layout/partials/_header_mobile.blade.php
@@ -2,7 +2,7 @@
     <div class="header-mobile__bar">
         <div class="container-fluid">
             <div class="header-mobile-inner">
-                <a class="logo" href="index.html">
+                <a class="logo" href="{{ route('admin_home_path') }}">
                     <img style="width:30%" src="{{asset('icons/favicon.png')}}" alt="{{env('APP_NAME')}}" />
                     <span class="ml-3" style="color:teal">{{ env('APP_NAME') }}</span>
                 </a>
@@ -18,29 +18,29 @@
         <div class="container-fluid">
             <ul class="navbar-mobile__list list-unstyled">
 
-                <li class="active has-sub">
-                    <a class="js-arrow" href="#">
+                <li class="{{set_active_route('admin_home_path')}} has-sub">
+                    <a class="js-arrow" href="{{ route('admin_home_path') }}">
                         <i class="fas fa-tachometer-alt"></i>Dashboard</a>
 
                 </li>
-                <li>
-                    <a href="chart.html">
+                <li class="{{set_active_route('admin_members_path')}}">
+                    <a href="{{route('admin_members_path')}}">
                         <i class="fas fa-users"></i>Membres</a>
                 </li>
-                <li>
-                    <a href="table.html">
+                <li class="{{set_active_route('admin_books_path')}}">
+                    <a href="{{route('admin_books_path')}}">
                         <i class="fas fa-book"></i>Livres</a>
                 </li>
                 <li>
-                    <a href="form.html">
+                    <a href="#">
                         <i class="far fa-comments"></i>Messages</a>
                 </li>
                 <li>
                     <a href="#">
                         <i class="fas fa-envelope"></i>Mails</a>
                 </li>
-                <li>
-                    <a href="map.html">
+                <li class="{{set_active_route('admin_profile_path')}}">
+                    <a href="{{route('admin_profile_path')}}">
                         <i class="fas fa-user"></i>Comptes</a>
                 </li>
             </ul>
