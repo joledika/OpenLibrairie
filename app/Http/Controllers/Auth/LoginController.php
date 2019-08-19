5d09ec5fea0254e6c63f1845cc8ed87ba79bc71f
Mamisoa <oliviermamisoa@gmail.com>
2019-08-19 19:14:11 +0300

    redirection sur un personne deja logé vers le dashboard

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -29,7 +29,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/membres';
+    protected $redirectTo = '/dashboard';
 
     /**
      * Create a new controller instance.
